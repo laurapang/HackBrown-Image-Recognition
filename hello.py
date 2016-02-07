4ee206e23ad1fbe6f0abc09b96049c971eb3f252
bisen <birolandhiscat@gmail.com>
2016-02-07 07:58:06 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from sample import match
+import sample
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return match()
+    return sample.match()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
