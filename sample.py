a65b1e6278501d7850eef46078d038c24de1df60
Laura Pang <lrpang@mit.edu>
2016-02-06 21:30:59 -0500

    verifying face (beta)

--- sample.py
+++ sample.py
@@ -9,11 +9,12 @@ import urllib, cStringIO, json
 from PIL import Image
 
 client = Client.Face('113bf51955964f02a2215d74f9b3078b') #API KEY
-file = cStringIO.StringIO(urllib.urlopen("http://172.18.141.55:8080/photo.jpg").read())
-img = Image.open(file)
-loc = "C:\\Users\\David\\Pictures\\test2.jpg"
-img.save(loc,"JPEG") #saving image
-#result1 = client.detect({'path': loc})
+#file = cStringIO.StringIO(urllib.urlopen("http://172.18.141.55:8080/photo.jpg").read())
+#img = Image.open(file)
+loc = "C:\\Users\\David\\Pictures\\laura_pang.jpg"
+#img.save(loc,"JPEG") #saving image
+snapId = client.detect({'path': loc})
+print snapId
 #result2 = client.detect({'url': 'https://upload.wikimedia.org/wikipedia/commons/1/19/Bill_Gates_June_2015.jpg'})
 #print (result)
 #test = client.verify('894a908c-6f38-48cd-a951-ee7dbde813d8', 'efedfac6-243e-4829-8502-3b2b932fe976')
@@ -26,3 +27,14 @@ urls = {}
 for person in data["contacts"]:
 	urls[person["name"]] = person["profile_image_url"]
 
+names = ['Aryan Chhabria','Laura Pang']
+dic = {}
+#dic['name'] = str(result2[0]['faceId'])
+for friend in names:
+    dic[friend]= client.detect({'url': urls[friend]})[0]['faceId']
+    print "%s %s" %(friend,dic[friend])
+for friend in names:
+    print friend
+    ver = client.verify(snapId[0]['faceId'], str(dic[friend]))
+    print ver
+    
